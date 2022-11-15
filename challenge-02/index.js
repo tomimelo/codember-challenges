f5910401e96c735af3db7d1e8aa6ebb942a4183b
Tomas Melone <tomasmelone@hotmail.com>
2022-11-16 00:01:51 +0100

    add alternative solution

--- challenge-02/index.js
+++ challenge-02/index.js
@@ -1,5 +1,7 @@
 import { loadData } from "../utils/dataLoader.js";
 
+const ASCII_REGEX = /1\d{2}|9\d{1}/g
+
 async function solver() {
   const encryptedMessage = await loadData("challenge-02/encrypted.txt");
 
@@ -12,15 +14,24 @@ async function solver() {
   return `submit ${decryptedMessage}`
 }
 
+
 function decryptWord(word) {
   //ASCII code of lowercase letters goes from 97 to 122. So if the code starts with 1 you need to take 3 characters, and if it starts with 9, you need to take only 2
-  const wordCharacters = word.match(/1\d{2}|9\d{1}/g)
-
+  const wordCharacters = word.match(ASCII_REGEX)
+  
   //Converting char array to it string equivalent. e.g: [ '116', '104', '97', '110', '107', '115' ] ==> 'thanks'
   return String.fromCharCode(...wordCharacters)
 }
 
+//Alternative solution in one line :D
+const alternativeSolver = async () => `submit ${(await loadData("challenge-02/encrypted.txt")).replace(ASCII_REGEX, (match) => String.fromCharCode(Number(match)))}`
+
 export const challenge02 = {
   name: "Challenge 02",
   solver,
+};
+
+export const challenge02b = {
+  name: "Challenge 02 (Alternative solution)",
+  solver: alternativeSolver,
 };
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { challenge01, challenge01b } from "./challenge-01/index.js";
-import { challenge02 } from "./challenge-02/index.js";
+import { challenge02, challenge02b } from "./challenge-02/index.js";
 
 async function solve(challenge) {
   console.log(`${challenge.name} ==> ${await challenge.solver()}`);
 }
 
-const challenges = [challenge01, challenge01b, challenge02];
+const challenges = [challenge01, challenge01b, challenge02, challenge02b];
 
 for (const challenge of challenges) {
   await solve(challenge);
