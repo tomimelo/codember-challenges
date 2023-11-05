9a10479e4b4f4e5d708b713cd334b7f13eecd5ac
Tomas Melone <tomasmelone@hotmail.com>
2023-11-05 21:52:38 +0100

    pull changes and fix conflicts

--- 2022/index.js
+++ 2022/index.js
@@ -2,6 +2,7 @@ import { challenge01, challenge01b } from "./challenge-01/index.js";
 import { challenge02, challenge02b } from "./challenge-02/index.js";
 import { challenge03 } from "./challenge-03/index.js";
 import { challenge04 } from "./challenge-04/index.js";
+import { challenge05 } from "./challenge-05/index.js";
 
 export const Challenges2022 = {
   challenge01, 
@@ -9,5 +10,6 @@ export const Challenges2022 = {
   challenge02, 
   challenge02b, 
   challenge03, 
-  challenge04
+  challenge04,
+  challenge05
 }
