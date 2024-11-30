cbdd52f53c1049404fc8ccfa22bf5ab0e68988f9
Tomas Melone <tomas.melone@es.stroeer-labs.com>
2024-11-30 12:53:01 +0100

    wip: add 2024 challenges

---
+++ 2024/index.js
@@ -0,0 +1,2 @@
+export const Challenges2024 = {
+}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { Challenges2022 } from "./2022/index.js";
 import { Challenges2023 } from "./2023/index.js";
+import { Challenges2024 } from "./2024/index.js";
 
 async function solve(challenge) {
   console.log(`${challenge.name} ==> ${await challenge.solver()}`);
@@ -13,6 +14,10 @@ const years = [
   {
     year: '2023',
     challenges: Object.values(Challenges2023)
+  },
+  {
+    year: '2024',
+    challenges: Object.values(Challenges2024)
   }
 ]
 
